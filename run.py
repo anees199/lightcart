caa4e220d4882db5573c56e8b6960257a0210203
anees199 <anees865577@gmail.com>
2026-06-30 15:23:03 -0700

    Update run.py to configure app to run on host and port from environment variables

--- run.py
+++ run.py
@@ -1,8 +1,20 @@
+# from app import create_app
+# from config import Config
+
+# app = create_app(Config)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
 from app import create_app
 from config import Config
+import os
 
 app = create_app(Config)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
